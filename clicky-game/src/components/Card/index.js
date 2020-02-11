0c1bddf84e0a1f90780b0af9943b5fb2e6c6efb4
Monica Amitin <monzielvsu@gmail.com>
2020-02-11 15:18:00 -0800

    created Card component

--- clicky-game/src/components/Card/index.js
+++ clicky-game/src/components/Card/index.js
@@ -1,2 +1,14 @@
 import React from "react";
-import "./style";
+import "./style.css";
+
+function card(props) {
+    return (
+        <div className="card" onClick={() => props.onClick(props.id)}>
+            <div className="img-container hvr-grow-rotate">
+                <img alt={props.name} src={props.image} />
+            </div>
+        </div>
+    );
+}
+
+export default card;
