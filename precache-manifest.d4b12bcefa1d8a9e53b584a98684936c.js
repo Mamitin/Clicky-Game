f5959d680ab048059848e225d5f2391228234294
Monica Amitin <monzielvsu@gmail.com>
2020-02-20 16:58:59 -0800

    Updates

---
+++ precache-manifest.d4b12bcefa1d8a9e53b584a98684936c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ac8e0c031dd8946a9acebbc0bcd02c",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "974ffccf4be8b4774764",
+    "url": "/Clicky-Game/static/css/main.6ffd04ed.chunk.css"
+  },
+  {
+    "revision": "92e68c3a1cfe74c9d40e",
+    "url": "/Clicky-Game/static/js/2.a1228723.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Clicky-Game/static/js/2.a1228723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "974ffccf4be8b4774764",
+    "url": "/Clicky-Game/static/js/main.4650bc67.chunk.js"
+  },
+  {
+    "revision": "ed33e96b23e5ad0b910f",
+    "url": "/Clicky-Game/static/js/runtime-main.da6617cf.js"
+  },
+  {
+    "revision": "9aec11f1e7dd12fdf789e3c380dd95e3",
+    "url": "/Clicky-Game/static/media/webb-dark.9aec11f1.png"
+  }
+]);
